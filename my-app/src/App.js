50f989e3901f3af05146d8992678a5a8c97345f4
sadsada <boyu@yahoo.com>
2020-08-10 22:51:26 +1000

    learned how to toogle switch between two modes dark and light by toggle the palette inside the theme

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SubApp from './Button/SubApp'
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import PaperPractice from './Paper/Paper'
+const theme = createMuiTheme({
+  // overrides: {
+  //   // Style sheet name ⚛️
+  //   MuiButton: {
+  //     // Name of the rule
+  //     text: {
+  //       // Some CSS
+  //       borderRadius: 3,
+  //       border: 0,
+  //       color: 'white',
+  //       height: 48,
+  //       padding: '0 30px',
+  //       boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+  //     },
+  //   },
+  // },
+});
 
+
+//Now i want change its style attribute  and add class....
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ThemeProvider theme={theme}>
+        <SubApp />
+        <PaperPractice />
+      </ThemeProvider>
     </div>
   );
 }
