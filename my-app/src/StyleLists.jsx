37d54f7dee02ddd6148c82d29332d2cae31b404e
sadsada <boyu@yahoo.com>
2020-09-11 13:38:01 +1000

    sds

--- my-app/src/StyleLists.jsx
+++ my-app/src/StyleLists.jsx
@@ -4,6 +4,8 @@ import { green } from '@material-ui/core/colors';
 export const useStyles = makeStyles({
     // We can make grouped style.... then it would be really useful... for example...
     // This way i think would be much easier for us to config className for the
+
+    //不是很理解为什么生成style 要把theme加进去。::
     loginTextStyle: {
         backgroundColor: 'gray',
         color: "blue",
@@ -12,6 +14,6 @@ export const useStyles = makeStyles({
 
     loginButtonStyle: {
         color: `green`,
-
     }
+
 });
